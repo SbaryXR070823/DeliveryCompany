573328e346606882ac62db6cd52bfb25612c25ab
Zdrimboi <162045987+Zdrimboi@users.noreply.github.com>
2024-04-04 21:46:50 +0300

    created OrderController (#11)
    created service for orders
    created order view model
    added function to get easier the userId
    added new project for the eventual helper functions
    added service in Program.cs
    added new navigation button on the navbar for the Orders page

---
+++ DeliveryCompany/Models/ViewModels/OrderVM.cs
@@ -0,0 +1,26 @@
+﻿using DeliveryCompany.Utility.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.ViewModels
+{
+    public class OrderVM
+    {
+        [Key]
+        public int Id { get; set; }
+        public double Price {  get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Weight { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+        public int Height { get; set; }
+        public DateTime DateTime { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
---
+++ DeliveryCompany/Services/Services/OrdersService.cs
@@ -0,0 +1,51 @@
+﻿using DeliveryCompany.DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Models.ViewModels;
+using Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly DataAppDbContext _dbContext;
+        public OrdersService(DataAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<OrderVM>> GetOrdersAsync(string userId)
+        {
+			List<OrderVM> orderViewModels = new List<OrderVM>();
+
+			var ordersList = await _dbContext.Orders
+				.Include(o => o.Packages) 
+				.Where(o => o.UserId == userId)
+				.ToListAsync();
+
+			foreach (var order in ordersList)
+			{
+				OrderVM orderViewModel = new OrderVM
+				{
+					Id = order.OrderId,
+					Price = order.Price,
+					Name = order.Packages.Name,
+					Description = order.Packages.Description,
+					Weight = order.Packages.Weight,
+					Width = order.Packages.Width,
+					Length = order.Packages.Length,
+					Height = order.Packages.Height,
+					DateTime = order.DateTime,
+					Status = order.OrderStatus
+				};
+				orderViewModels.Add(orderViewModel);
+			}
+
+			return orderViewModels;
+		}
+    }
+}
